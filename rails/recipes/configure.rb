fc6f3768807736e191b5a7a37cddd8886e99649a
jtsaito <jahn.saito@gmail.com>
2013-11-01 15:24:58 +0100

    Removes analyst.yml from shared directory.

--- rails/recipes/configure.rb
+++ rails/recipes/configure.rb
@@ -45,24 +45,6 @@ node[:deploy].each do |application, deploy|
         end
     end
 
-    template "#{deploy[:deploy_to]}/shared/config/analyst.yml" do
-        source "analyst.yml.erb"
-        cookbook 'rails'
-        mode "0660"
-        group deploy[:group]
-        owner deploy[:user]
-        variables(
-                  :analyst => deploy[:analyst] || {},
-                  :environment => deploy[:rails_env]
-                  )
-
-        notifies :run, "execute[restart Rails app #{application}]"
-
-        only_if do
-            File.exists?("#{deploy[:deploy_to]}") && File.exists?("#{deploy[:deploy_to]}/shared/config/")
-        end
-    end
-
     template "#{deploy[:current_path]}/config/analyst.yml" do
         source "analyst.yml.erb"
         cookbook 'rails'
